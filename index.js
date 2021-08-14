8aee22e6a876dfc64e7c5ad5142b56cf41bb70ff
Tania Tatula <tgrogol@gmail.com>
2021-08-14 13:31:22 +0300

    refactoring code

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 const {
-    listContacts,
-    getContactById,
-    removeContact,
-    addContact
-  } = require('./contacts');
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts");
 
-// console.log(addContact('Mango', 'mango@gmail.com', '322-22-22'));
-
-
-const { Command } = require('commander');
+const { Command } = require("commander");
 const program = new Command();
 program
-  .option('-a, --action <type>', 'choose action')
-  .option('-i, --id <type>', 'user id')
-  .option('-n, --name <type>', 'user name')
-  .option('-e, --email <type>', 'user email')
-  .option('-p, --phone <type>', 'user phone');
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
 
 program.parse(process.argv);
 
@@ -24,30 +21,33 @@ const argv = program.opts();
 // TODO: рефакторить
 function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
-    case 'list':
-        console.table(listContacts());
+    case "list":
+      listContacts();
       break;
 
-    case 'get':
-        if(argv.id){
-            getContactById(id);
-        }
-      else console.log('id not specified');
+    case "get":
+      if (argv.id) {
+        getContactById(id);
+      } else console.log("id not specified");
       break;
 
-    case 'add':
-      console.log(addContact(name, email, phone));
+    case "add":
+      if ((argv.name, argv.email, argv.phone)) {
+        addContact(name, email, phone);
+      } else console.log("name, email or phone not specified");
+
       break;
 
-    case 'remove':
-       removeContact(id);
-        break;
+    case "remove":
+      if (argv.id) {
+        removeContact(id);
+      } else console.log("id not specified");
+
+      break;
 
     default:
-      console.warn('\x1B[31m Unknown action type!');
+      console.warn("\x1B[31m Unknown action type!");
   }
 }
 
 invokeAction(argv);
-
-// console.log(getContactById(5));
